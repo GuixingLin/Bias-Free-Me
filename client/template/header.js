95eb5de7402f03960debea4a7b358fd12ab0c8a8
jeffgyf <jeffguo@umich.edu>
2017-11-03 12:13:02 -0700

    initial

---
+++ client/template/header.js
@@ -0,0 +1,9 @@
+Template.header.events({
+  'click button#chatb'(e) {
+    document.getElementById('frame').src="chat.html";
+  },
+  'click button#newsb'(e) {
+    document.getElementById('frame').src="news.html";
+  }
+});
+    
